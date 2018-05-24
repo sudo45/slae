c3294c2528b40ac65665946017df665bc60a96c5
sudo45 <luca94dd@gmail.com>
2018-05-24 09:29:26 +0200

    Add files via upload
    fixed bug if port number has less than 4 hexadecimal digits

--- assignement-1/change_port.py
+++ assignement-1/change_port.py
@@ -15,7 +15,7 @@ if port < 1024 or port > 65535:
     sys.exit(-1)
 
 no_port = socket.htons(port);
-hex_no_port = hex(no_port)
+hex_no_port = "{0:#0{1}x}".format(no_port,6)
 
 h1 = hex_no_port[2:4]
 h2 = hex_no_port[4:6]
